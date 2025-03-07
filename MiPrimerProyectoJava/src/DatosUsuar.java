310828ceeb42233e00cffff448aea61d30f00569
Andrey Galvis <andreygalvis6002@gmail.com>
2025-03-07 09:55:33 -0500

    Ejercicios practicos

---
+++ MiPrimerProyectoJava/src/DatosUsuar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DatosUsuar {
+
+    public static void main(String[] args) {
+       System.out.println("ingrese tres numeros:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese el primer numero: ");
+        int num1 = scanner.nextln();
+        System.out.println("ingrese el segundo numero:");
+        int num2 = scanner.nextln();
+        System.out.println("ingrese el tercer numero:");
+        int num3 = scanner.nextln();
+        boolean reultedo = (num1>num2) && (num1<num3);
+        System.out.println("cuales numeros son mayores :");
+        
+
+
+    }
---
+++ MiPrimerProyectoJava/src/DatosUsuario.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DatosUsuario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor, ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Por favor, ingresa tu edad: ");
+        int edad = scanner.nextInt();
+        System.out.print("Por favor, ingresa tu altura en metros (por ejemplo, 1.75): ");
+        double altura = scanner.nextDouble();
+        System.out.println("\nHola, " + nombre + "!");
+        System.out.println("Tienes " + edad + " años y mides " + altura + " metros de altura.");
+        scanner.close();
+    }
+}
