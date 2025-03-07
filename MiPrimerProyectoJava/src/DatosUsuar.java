5d5a869d7a9be7bc081144c828287258b3432dee
Andrey Galvis <andreygalvis6002@gmail.com>
2025-03-07 11:32:03 -0500

    git

--- MiPrimerProyectoJava/src/DatosUsuar.java
+++ MiPrimerProyectoJava/src/DatosUsuar.java
@@ -13,7 +13,7 @@ public class DatosUsuar {
         int num3 = scanner.nextln();
         boolean reultedo = (num1>num2) && (num1<num3);
         System.out.println("cuales numeros son mayores :");
-        
-
-
+        if (i=1;1<10){
+            
+        }
     }
