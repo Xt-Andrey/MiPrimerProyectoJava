6eb2dfa5036c99a7e8e37a1a364d3c11405519a5
Andrey Galvis <andreygalvis6002@gmail.com>
2025-03-28 09:05:07 -0500

    git

--- MiPrimerProyectoJava/MiPrimerProyectoJava/src/Ejer13.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Ejer13 {
-public static void main(String[] args) {
-    Scanner miobj=new Scanner(System.in);
-    for (int i = 1; i <= 100; i++) {
-        System.out.println("los numeros son :"+i);
-    }
-}    
-}
--- MiPrimerProyectoJava/MiPrimerProyectoJava/src/Ejer14.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Ejer14 {
-public static void main(String[] args) {
-    Scanner miobj=new Scanner(System.in);
-    for (int i =0; i <= 10; i++){
-        System.out.println("la tabla del 5 es "+i*5);
-    }
-}    
-}
